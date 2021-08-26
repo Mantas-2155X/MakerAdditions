72803f49d3bb1f80d2d85ad2fefc8e29197673e4
Mantas-2155X <13873974+Mantas-2155X@users.noreply.github.com>
2021-08-27 01:35:54 +0300

    Port to KKS NuGet

---
+++ KKS_MakerAdditions/KKS_MakerAdditions.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+using KKAPI.Maker;
+using KKAPI.Maker.UI.Sidebar;
+
+using UniRx;
+using UnityEngine;
+
+namespace KKS_MakerAdditions
+{
+    [BepInProcess("KoikatsuSunshine")]
+    [BepInProcess("KoikatsuSunshineTrial")]
+    [BepInPlugin(nameof(KKS_MakerAdditions), nameof(KKS_MakerAdditions), VERSION)]
+    public class KKS_MakerAdditions : BaseUnityPlugin
+    {
+        public const string VERSION = "1.0.0";
+        
+        public new static ManualLogSource Logger;
+        
+        private static GameObject oldParent;
+        private static GameObject newParent;
+        
+        private SidebarToggle lockCamlightToggle;
+        
+        private void Awake()
+        {
+            Logger = base.Logger;
+
+            var harmony = new Harmony(nameof(KKS_MakerAdditions));
+
+            var moreAccsPos = Type.GetType("MoreAccessoriesKOI.ChaControl_SetAccessoryPos_Patches, MoreAccessories, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+            var moreAccsRot = Type.GetType("MoreAccessoriesKOI.ChaControl_SetAccessoryRot_Patches, MoreAccessories, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+            
+            if (moreAccsPos != null && moreAccsRot != null)
+            {
+                harmony.Patch(moreAccsPos.GetMethod("Prefix", AccessTools.all), null, null, new HarmonyMethod(typeof(Hooks), nameof(Hooks.ChaControl_SetAccessoryPos_ChangeLimit)));
+                harmony.Patch(moreAccsRot.GetMethod("Prefix", AccessTools.all), null, null, new HarmonyMethod(typeof(Hooks), nameof(Hooks.ChaControl_SetAccessoryRot_ChangeLimit)));
+            }
+            
+            harmony.PatchAll(typeof(Hooks));
+            
+            MakerAPI.MakerBaseLoaded += MakerAPI_Enter;
+            MakerAPI.MakerExiting += (_, __) => OnDestroy();
+        }
+        
+        private void MakerAPI_Enter(object sender, RegisterCustomControlsEvent e)
+        {
+            var camLight = GameObject.Find("CustomScene/CamBase/Camera/Directional Light").transform;
+            
+            lockCamlightToggle = e.AddSidebarControl(new SidebarToggle("Lock Cameralight", false, this));
+            lockCamlightToggle.Value = false;
+            lockCamlightToggle.ValueChanged.Subscribe(x =>
+            {
+                if (camLight == null)
+                    return;
+
+                if (x)
+                {
+                    oldParent = camLight.parent.gameObject;
+
+                    newParent = new GameObject("CamLightLock");
+                    newParent.transform.position = oldParent.transform.position;
+                    newParent.transform.eulerAngles = oldParent.transform.eulerAngles;
+
+                    camLight.parent = newParent.transform;
+                }
+                else if(oldParent != null)
+                {
+                    camLight.parent = oldParent.transform;
+
+                    camLight.transform.localEulerAngles = new Vector3(0, 3, 0);
+
+                    Destroy(newParent);
+                    newParent = null;
+                }
+            });
+        }
+
+        private void OnDestroy()
+        {
+            lockCamlightToggle = null;
+        }
+    }
+}
---
+++ KKS_MakerAdditions/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KKS_MakerAdditions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mantas-2155X aka 2155X")]
+[assembly: AssemblyProduct("KKS_MakerAdditions")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94B2F800-C748-45E4-8AD1-0F08D601B931")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(KKS_MakerAdditions.KKS_MakerAdditions.VERSION)]
+[assembly: AssemblyFileVersion(KKS_MakerAdditions.KKS_MakerAdditions.VERSION)]
